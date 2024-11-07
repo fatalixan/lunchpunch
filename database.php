8d1e12304de94922a6622d74a16af4ee62524e17
fatalixan <fatalixan@gmail.com>
2024-11-07 23:26:18 +0300

    +vercel.json

--- database.php
+++ database.php
@@ -1,21 +1,13 @@
 
 <?php
- POSTGRES_URL="postgres://default:FL0ykw3vBEtb@ep-snowy-dust-a47wvub0-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
- POSTGRES_PRISMA_URL="postgres://default:FL0ykw3vBEtb@ep-snowy-dust-a47wvub0-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require&pgbouncer=true&connect_timeout=15"
- POSTGRES_URL_NO_SSL="postgres://default:FL0ykw3vBEtb@ep-snowy-dust-a47wvub0-pooler.us-east-1.aws.neon.tech:5432/verceldb"
- POSTGRES_URL_NON_POOLING="postgres://default:FL0ykw3vBEtb@ep-snowy-dust-a47wvub0.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
- POSTGRES_USER="default"
- POSTGRES_HOST="ep-snowy-dust-a47wvub0-pooler.us-east-1.aws.neon.tech"
- POSTGRES_PASSWORD="FL0ykw3vBEtb"
- POSTGRES_DATABASE="verceldb"
 
 function connectDatabase() {
 
 
-    $host = POSTGRES_HOST;
-    $dbname = POSTGRES_DATABASE="verceldb";
-    $user = POSTGRES_USER;
-    $password = POSTGRES_PASSWORD
+    $host = $_ENV['POSTGRES_HOST'];
+    $db = $_ENV['POSTGRES_DATABASE'];
+    $user = $_ENV['POSTGRES_USER'];
+    $password = $_ENV['POSTGRES_PASSWORD'];   
 
     try {
         $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
