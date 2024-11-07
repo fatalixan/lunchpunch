6242ddd4a801ff02c24f6ce504064eae2be73c1e
fatalixan <fatalixan@gmail.com>
2024-11-07 23:14:39 +0300

    Update database.php

--- database.php
+++ database.php
@@ -1,24 +1,29 @@
+
 <?php
-// Подключаемся к базе данных PostgreSQL с помощью PDO
-try {
-    // Получаем параметры подключения из переменных окружения
-    $host = getenv('DB_HOST');
-    $dbname = getenv('DB_NAME');
-    $user = getenv('DB_USER');
-    $password = getenv('DB_PASS');
-    $port = getenv('DB_PORT');
+ POSTGRES_URL="postgres://default:FL0ykw3vBEtb@ep-snowy-dust-a47wvub0-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
+ POSTGRES_PRISMA_URL="postgres://default:FL0ykw3vBEtb@ep-snowy-dust-a47wvub0-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require&pgbouncer=true&connect_timeout=15"
+ POSTGRES_URL_NO_SSL="postgres://default:FL0ykw3vBEtb@ep-snowy-dust-a47wvub0-pooler.us-east-1.aws.neon.tech:5432/verceldb"
+ POSTGRES_URL_NON_POOLING="postgres://default:FL0ykw3vBEtb@ep-snowy-dust-a47wvub0.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require"
+ POSTGRES_USER="default"
+ POSTGRES_HOST="ep-snowy-dust-a47wvub0-pooler.us-east-1.aws.neon.tech"
+ POSTGRES_PASSWORD="FL0ykw3vBEtb"
+ POSTGRES_DATABASE="verceldb"
 
-    // Создаем строку подключения (DSN)
-    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
+function connectDatabase() {
 
-    // Устанавливаем подключение
-    $pdo = new PDO($dsn, $user, $password);
 
-    // Устанавливаем режим ошибок (опционально)
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $host = POSTGRES_HOST;
+    $dbname = POSTGRES_DATABASE="verceldb";
+    $user = POSTGRES_USER;
+    $password = POSTGRES_PASSWORD
 
-    echo "Подключение к базе данных успешно!";
-} catch (PDOException $e) {
-    echo "Ошибка подключения к базе данных: " . $e->getMessage();
+    try {
+        $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+        exit();
+    }
 }
 ?>
